054771388afa4a9340ca9a7f3fd1dd83009f1a1d
Insoi <wzy87hky123@gmail.com>
2026-02-06 02:43:46 +0100

    same as before, more organized

--- scripts/globals/config.gd
+++ scripts/globals/config.gd
@@ -3,10 +3,10 @@ extends Node
 const board_height : int = 10
 const board_width : int = 10
 const cell_size : int = 16
-const starting_pos : Vector2i = Vector2i(5,5) # * cell_size
+const starting_pos : Vector2i = Vector2i(5,6) # * cell_size
 
 @warning_ignore("integer_division")
-const bombs : int = (board_height * board_width / 100) * 25 # as in 20% of the total cells on the board
+const bombs : int = (board_height * board_width / 100) * 15 # as in 20% of the total cells on the board
 
 const numbers = [14, 13, 12, 11, 10, 9, 8, 7]
 const unexplored_cell = 0
