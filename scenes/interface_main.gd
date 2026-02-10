834a9ac81bc92d5c379c918ab69da52c781559a7
Insoi <wzy87hky123@gmail.com>
2026-02-10 02:25:43 +0100

    interface auto scaling

--- scenes/interface_main.gd
+++ scenes/interface_main.gd
@@ -2,4 +2,11 @@ extends Node2D
 
 @warning_ignore("unused_parameter")
 func _on_board_generated(cell_array: Array) -> void:
-	position = Vector2i(Config.BOARD_WIDTH)
+	var x_center = (Config.STARTING_POS.x + Config.BOARD_WIDTH / 2.0) * Config.CELL_SIZE
+	position = Vector2(x_center, (Config.STARTING_POS.y - 0.8) * Config.CELL_SIZE)
+	
+	var board_pixel_width = Config.BOARD_WIDTH * Config.CELL_SIZE
+	var available_width = get_viewport_rect().size.x - (0.8 * Config.CELL_SIZE * 2)
+	
+	var target_scale = (board_pixel_width * Config.INTERFACE_MARGIN) / available_width
+	scale = Vector2(target_scale, target_scale)
--- scripts/globals/config.gd
+++ scripts/globals/config.gd
@@ -1,16 +1,20 @@
 extends Node
 
-const BOARD_HEIGHT : int = 8
-const BOARD_WIDTH : int = 8
+# GENERAL / BOARD
+const BOARD_HEIGHT : int = 10
+const BOARD_WIDTH : int = 15
 const CELL_SIZE : int = 16
-
-const TOP_MARGIN : int = 4 # how much the top dark background layer should go down
-const BOTTOM_MARGIN : int = 5 # how many cells should be removed on the y axis for background generation / window
-const STARTING_POS : Vector2i = Vector2i(4,9) # starting_pos * cell_size and starting_pos starts at 0
-
 @warning_ignore("narrowing_conversion")
 const BOMBS : int = (float(BOARD_HEIGHT * BOARD_WIDTH) / 100.0) * 14 # as in 20% of the total cells on the board
 
-# debugging constants
+# BACKGROUND
+const TOP_MARGIN : int = 3 # how much the top dark background layer should go down
+const BOTTOM_MARGIN : int = 4 # how many cells should be removed on the y axis for background generation / window
+const STARTING_POS : Vector2i = Vector2i(4,7) # starting_pos * cell_size and starting_pos starts at 0
+
+@warning_ignore("narrowing_conversion")
+const INTERFACE_MARGIN : float = 1.9
+
+# DEBUGGING
 const DEBUG_TILES : bool = false
 const DEBUG_BOARD : bool = true
